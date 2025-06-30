0e4a5a24d6636620e2d70a019e49254c100a7f67
Purushottam Singh <147805292+purushottamsingh1141@users.noreply.github.com>
2025-06-30 12:00:26 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 // Enable CORS for localhost frontend (React)
 const corsOptions = {
-  origin:["https://foodie-oppwd772a-purushottam-singhs-projects.vercel.app"],
+  origin:"*",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true, 
