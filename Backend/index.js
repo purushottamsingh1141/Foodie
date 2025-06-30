b11fa23775732fb3a9539ee7ac7b8d71dc927df7
Purushottam Singh <147805292+purushottamsingh1141@users.noreply.github.com>
2025-06-30 11:35:32 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,13 +11,14 @@ mongoDB();
 const port = process.env.PORT || 5000;
 
 // Enable CORS for localhost frontend (React)
-app.use(
-  cors({
-    origin: ["https://foodie-oppwd772a-purushottam-singhs-projects.vercel.app/"],
-    method: ["GET", "POST", "PUT", "PATCH"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin:["https://foodie-oppwd772a-purushottam-singhs-projects.vercel.app"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true, 
+ };
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
