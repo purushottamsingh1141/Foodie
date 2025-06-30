0858a8e6680b65b7ecdb692a2ea14949daaca71b
Purushottam Singh <147805292+purushottamsingh1141@users.noreply.github.com>
2025-06-30 10:46:21 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -18,7 +18,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://foodie-g41l.onrender.com/api/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
